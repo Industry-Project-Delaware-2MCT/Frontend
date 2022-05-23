0c1f40465edf38cd678c726b28806f51c36f91d4
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-05-23 18:27:53 +0200

    fixed login

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+const init = () => {
+    console.log("init");
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        /*if(!isValid){
+            console.log("wooh");
+            e.preventDefault();   
+        }*/
+        Login();
+    });
+    
+
+}
+
 const Login = async () => {
+    
     if(checkInputs()) {
         authenticate(firstName.value, lastName.value);
     }
@@ -19,10 +34,29 @@ const authenticate = async (firstname, lastname) => {
         },
         body: JSON.stringify(data),
     })
-    .then(response => response.json())
+    .then(response => checkLogin(response))
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 }
+
+
+
+const checkLogin = (json) => {
+    console.log(json);
+
+    if(json.status == 200) {
+        console.log("login succes");
+        console.log(window.location.href);
+        window.location.href = "http://localhost:5500/Frontend/index.html";
+    } else {
+        console.log("login failed");
+        console.log(json.status);
+        /*firstName.classList.add('c-empty_input');
+        lastName.classList.add('c-empty_input');*/
+    }
+
+}
+
 
 const removeWrongInput = () => {
     firstName.classList.remove('c-empty_input');
@@ -47,7 +81,14 @@ const checkInputs = () => {
     return check;
 }
 
+
+
 document.addEventListener('DOMContentLoaded', async function () {
 	firstName = document.querySelector('.js-firstname');
 	lastName = document.querySelector('.js-lastname'); 
+    loginForm = document.querySelector('.js-login');
+    if(loginForm) {
+        console.log("loginform is not empty");
+    }
+    init();
 });
