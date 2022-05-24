d7b4b07dc0355f55b59ee23509138d4753c3e6c1
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-05-24 15:07:51 +0200

    added scan functionality with backend

--- js/script.js
+++ js/script.js
@@ -15,6 +15,9 @@ const init = () => {
     }else if(window.location.href.includes("PatientPage.html")) {
         greeting = document.querySelector('.js-greeting');
         showPatientName();
+    } else if(window.location.href.includes("ScanPage.html")) {
+        errorMessage = document.querySelector(".js-error");
+        scan();
     }
 }
 
@@ -52,6 +55,25 @@ const authenticate = async (firstname, lastname) => {
     .catch(error => console.log('error', error));
 }
 
+const authenticateById = async (id) => {
+    var data = {
+        id: id
+    };
+
+    console.log("fetching");
+
+    fetch("https://industryprojectapi.azurewebsites.net/api/authenticatie/id", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => checkLogin(response))
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
 
 
 const checkLogin = (json) => {
@@ -70,6 +92,8 @@ const checkLogin = (json) => {
     }
 
 }
+
+
 
 const showPatientName = () => {
     nurseName = localStorage.getItem("firstname");
@@ -101,7 +125,28 @@ const checkInputs = () => {
     return check;
 }
 
-
+const scan = async () => {
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      console.log("Scanning");
+  
+      ndef.addEventListener("readingerror", () => {
+        console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+        errorMessage.innerHTML = "Scan niet gelukt, probeer opnieuw of log manueel in";
+      });
+  
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        authenticateById(serialNumber);
+        console.log(`Serial Number: ${serialNumber}`);
+        console.log(`Records: (${message.records.length})`);
+        console.log(`Records: (${message.records})`);
+      });
+    } catch (error) {
+      console.log("Argh! " + error);
+      errorMessage.innerHTML = "Er is iets fout gegaan, probeer opnieuw of log manueel in";
+    }
+  };
 
 document.addEventListener('DOMContentLoaded', async function () {
     init();
