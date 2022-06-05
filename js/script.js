1bff9d62cf2a871ef28914dfbb6a0f4c2b2894ac
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-06-05 22:59:29 +0200

    PWA should work added manifest and service worker

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./sw.js')
+    .then(function() { 
+        console.log('Service Worker Registered'); 
+    });
+}
+
 let nurseName = "test";
 
 const init = () => {
