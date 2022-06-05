dba9cb5ebac991d6bb8c8d3714b8f945bf2c6e10
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-06-06 00:05:19 +0200

    changed to network first caching

--- sw.js
+++ sw.js
@@ -1,52 +1,120 @@
-const cacheName = 'mediscan-v1';
-const staticAssets = [
-  './',
-  'index.html',
-  'LoginPage.html',
-  'MedicationPage.html',
-  'NFCpage.html',
-  'PatientInfoPage.html',
-  'PatientPage.html',
-  '../Frontend/css/screen.css',
-  '../Frontend/css/normalize.css',
-  '../Frontend/js/script.js'
-];
+// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+// these are the routes we are going to cache for offline support
+const cacheFiles = ['/', '/about-me/', '/projects/', '/offline/'];
+
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  )
+);
+
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || cache.match('/offline/'))
+    );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
+  );
+  evt.waitUntil(update(evt.request));
 });
+// const cacheName = 'mediscan-v1';
+// const staticAssets = [
+//   './',
+//   'index.html',
+//   'LoginPage.html',
+//   'MedicationPage.html',
+//   'NFCpage.html',
+//   'PatientInfoPage.html',
+//   'PatientPage.html',
+//   '../Frontend/css/screen.css',
+//   '../Frontend/css/normalize.css',
+//   '../Frontend/js/script.js'
+// ];
 
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
+// self.addEventListener('install', async e => {
+//   const cache = await caches.open(cacheName);
+//   await cache.addAll(staticAssets);
+//   return self.skipWaiting();
+// });
 
-self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
+// self.addEventListener('activate', e => {
+//   self.clients.claim();
+// });
 
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
-});
+// self.addEventListener('fetch', async e => {
+//   const req = e.request;
+//   const url = new URL(req.url);
 
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
-}
+//   if (url.origin === location.origin) {
+//     e.respondWith(cacheFirst(req));
+//   } else {
+//     e.respondWith(networkAndCache(req));
+//   }
+// });
 
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
+// async function cacheFirst(req) {
+//   const cache = await caches.open(cacheName);
+//   const cached = await cache.match(req);
+//   return cached || fetch(req);
+// }
+
+// async function networkAndCache(req) {
+//   const cache = await caches.open(cacheName);
+//   try {
+//     const fresh = await fetch(req);
+//     await cache.put(req, fresh.clone());
+//     return fresh;
+//   } catch (e) {
+//     const cached = await cache.match(req);
+//     return cached;
+//   }
+// }
