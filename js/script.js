026e4d4d9b0df49ade65884196e0129ba49e338e
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-06-07 20:57:57 +0200

    made medicationNames public variable

--- js/script.js
+++ js/script.js
@@ -490,7 +490,7 @@ const getMedicationData = async (base64image) => {
     
     var medication = JSON.parse(localStorage.getItem("medication"));
     var checkmark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
-    let medicationNames = [];
+    medicationNames = [];
     medication.forEach(item => {
         medicationNames.push(item.medication_name);
     });
