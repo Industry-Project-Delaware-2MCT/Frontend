62fc82b81e71ba8cf63a6ae7dea3457ab263feeb
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-06-07 17:02:47 +0200

    changed item.name to item.medication_name

--- js/script.js
+++ js/script.js
@@ -492,7 +492,7 @@ const getMedicationData = async (base64image) => {
     var checkmark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
     let medicationNames = [];
     medication.forEach(item => {
-        medicationNames.push(item.name);
+        medicationNames.push(item.medication_name);
     });
     console.log("medications: ",medicationNames);
     console.log("medication: ",medication);
