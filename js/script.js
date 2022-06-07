cb41d3e2e6518c39cf55a3571d283c328e058eaf
De Tandt Thibaut <thibaut.de.tandt@student.howest.be>
2022-06-07 16:55:56 +0200

    adjusted request body for medication

--- js/script.js
+++ js/script.js
@@ -490,10 +490,14 @@ const getMedicationData = async (base64image) => {
     
     var medication = JSON.parse(localStorage.getItem("medication"));
     var checkmark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
+    let medicationNames = [];
+    medication.forEach(item => {
+        medicationNames.push(item.name);
+    });
     
     var data = {
         base64String: base64image,
-        active_substance: medication[0].medication_name,
+        active_substance:  medicationNames,
     };
     
     camera = document.querySelector(".js-camera");
@@ -719,6 +723,8 @@ const scan = async () => {
         title.innerHTML = "NFC staat niet aan";
     }
   };
+
+
 /*==========================
 BARCODE FUNCTIONALITY
 ===========================*/
